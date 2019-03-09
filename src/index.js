bb4275329b6ebdd45f1666ef5b4fe64e452a4217
Jorge Monroy <jlmonroy13@gmail.com>
2019-03-08 21:49:06 -0500

    Notes (ComponentWillUnmount)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const unmountButton = document.getElementById('unmount');
+
+// Is not very common to remove a Component from the DOM, // but this will be just to understand how
+// componentWillUnmount works.
+function unmount() {
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+  document.getElementById('unmountMessage').style.display = 'block';
+  unmountButton.remove();
+}
+unmountButton.addEventListener('click', unmount);
+document.getElementById('unmountMessage').style.display = 'none';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
