b8f9cf9efe9f77d40efff0296a035232e52f8b8f
Jorge Monroy <jlmonroy13@gmail.com>
2019-03-10 11:13:29 -0500

    Using pure component

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,20 @@
 import React, { Component } from 'react';
-import Animation from './Animation/Animation';
+import Numbers from './Numbers/Numbers';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
 import './App.css';
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { chartType: 'line' };
-    this.columns = [
-      ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
-      ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
-      ['XRP', 100, 200, 300, 500, 400, 300]
-    ];
-  }
-  setBarChart = () => {
-    this.setState({
-      chartType: 'bar'
-    });
-  };
-  setLineChart = () => {
-    this.setState({
-      chartType: 'line'
-    });
-  };
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title="Charts" />
+        <Header title="Understanding Pure Components" />
         <Content>
-          {' '}
-          <Animation />
+          <Numbers />
         </Content>
         <Footer />
       </div>
     );
   }
 }
+export default App;
---
+++ src/components/Numbers/Result.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+class Result extends PureComponent {
+  render() {
+    return <li>{this.props.result}</li>;
+  }
+}
+export default Result;
