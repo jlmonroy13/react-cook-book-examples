2c98da9e1b5fca2412152ad9b14f10c73d0748b8
Jorge Monroy <jlmonroy13@gmail.com>
2019-03-10 12:41:37 -0500

    XSS vulnerabilities

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Numbers from './Numbers/Numbers';
+import Xss from './Xss/Xss';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Header title="Understanding Pure Components" />
         <Content>
-          <Numbers />
+          <Xss />
         </Content>
         <Footer />
       </div>
